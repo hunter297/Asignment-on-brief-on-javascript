aad33395e571382aafeb86a9bc31c23382d20d7a
hunter297 <edidimubarak@gmail.com>
2024-12-29 21:29:01 +0100

    assignment on breif in javascript

---
+++ script.js
@@ -0,0 +1,44 @@
+//Assignment on javascript
+
+
+var F = 20.93933
+var E = "Trains"
+var A = null
+var R = true
+
+
+document.write(typeof (F))
+document.write("<br>")
+document.write(typeof (E))
+document.write("<br>")
+document.write(typeof (A))
+document.write("<br>")
+document.write(typeof (R))
+document.write("<br>")
+
+
+var s = true
+
+var stadium = ["metropolitano stadium", "camp nou", "Santiago bernabaeu"]
+var age = 40
+if (age >= 20) {
+    document.write("You are not allowed to view this content if not your computer will crash")
+}
+
+else {
+    document.write("you are allowed to view this content")
+}
+document.write("<br>")
+let i = 1
+while (i <=45){
+    document.write(i)
+    i++;
+}
+
+for (i = 100; i <=200000; i++) {
+    document.write(i + "<br>");
+}
+
+
+
+
